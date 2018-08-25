f3ff43319e99641e2989f2f2452735cbd5ca5fad
Rohan890 <league@wts7.attlocal.net>
2018-08-25 12:11:26 -0700

    Finally finished module 3.

---
+++ src/loops/YearsAlive.java
@@ -0,0 +1,19 @@
+package loops;
+
+	
+public class YearsAlive {
+	
+	public static void main(String[] args) {
+int year = 2006;	
+	for (int i = 0; i < 12; i++) {
+		year++;
+		System.out.println(year);
+	}
+		
+
+
+
+
+}
+	
+}
